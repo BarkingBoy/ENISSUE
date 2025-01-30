c7114b51b62fe2143037da6df09416db2a4723ae
Feudai <tim.criaud@gmail.com>
2025-01-30 13:46:05 +0100

    Adaptation for Mongoose

--- app.js
+++ app.js
@@ -5,20 +5,26 @@ const port = process.env.PORT || 3000;
 
 require("dotenv").config();
 
-const uri = "mongodb://localhost:27017/enissue";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-const db = client.db("enissue");
-
-client
-  .connect()
-  .then(() => {
-    console.log("Connected successfully to server");
+mongoose
+  .connect("mongodb://localhost:27017/enissue", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
-  .catch((err) => {
-    console.log("Error connecting to server:", err);
+  .then(() => console.log("Connected successfully to server"))
+  .catch((err) => console.log("Error connecting to server:", err));
+
+  //Schema mongoose
+  const issueSchema = new mongoose.Schema({
+    auteur: auteur,
+    probleme: probleme,
+    description: description,
+    Etat: "Nouveau",
+    dateCrea: new Date().toLocaleDateString(),
+    id: id,
   });
 
-
+//Modele
+const Issue = mongoose.model('Issue',issueSchema);
 
 // Configuration
 app.set("views", "./public/views");
@@ -26,58 +32,93 @@ app.set("view engine", "ejs");
 app.use(express.static("public")); // For serving static files
 app.use(express.urlencoded({ extended: true }));
 
-let issues = [];
 
 // Routes
-app.get("/", (req, res) => {
-  
-    db.collection('issues').find().toArray().then((issues)=>{
-res.render("index.ejs", {issues:issues});
+app.get("/", async (req, res) => {
+  try{
+    const issues = await Issue.find({});
+res.render("index.ejs", {issues});
+    } catch(err){
+      next(err);
+    }
    });
+
+
+app.post("/views/create", async (req, res, next) => {
+ try{
+  const { auteur, probleme, description } = req.body;
+  const lastIssue = await Issue.findOne().sort({ id: -1 });
+  const nextId = lastIssue ? lastIssue.id + 1 : 0;
+   await Issue.create({
+    auteur,
+    probleme,
+    description,
+    id: nextId,
+  });
+    res.redirect("/");
+}catch(err){
+  next(err);
+}
 });
 
+//details
+app.get("/views/detail/:id", async (req, res, next) => {
+  try{
+  const issueId = parseInt(req.params.id);
+  const issue = await issue.findOne({id:issueId});
 
-app.post("/views/create", (req, res) => {
+  if(!issue){
+    return res.status(404).redirect('/404');
+  }
+    
+  res.render("detail", { issue });
+  } catch(err){
+    next(err);
+  }
+});
+
+//update
+app.post("/issues/detail/:id", async (req, res, next) => {
+  try{
+  const issueId = parseInt(req.params.id);
   const { auteur, probleme, description } = req.body;
-   db.collection('issues').insertOne({
-    auteur:auteur,
-    probleme:probleme,
-    description:description,
-    Etat: "Nouveau", 
-    dateCrea: new Date().toLocaleDateString(), 
-    id: issues.length++,
+
+  const response = await Issue.updateOne(
+  {id:issueId},
+  {
+    set:{
+    auteur,
+    probleme,
+    description,
+    }
   });
 
-  console.log(issues);
+  if(response.modifiedCount === 0){
+        return res.status(404).redirect("/404");
+  }
+
   res.redirect("/");
+}catch(err){
+  next(err);
+}
 });
 
-app.get("/views/detail/:id", (req, res) => {
-  const issueId = req.params.id;
-    
-  res.render("detail", { issue: issues[issueId] });
-});
-// app.post("/issues/detail/:id", (req, res) => {
-//   const { auteur, probleme, description } = req.body;
-//   issues.push({
-//     auteur,
-//     probleme,
-//     description,
-//   });
-//   res.redirect("/");
-// });
 
-
-app.post("/issues/delete", (req, res) => {
+app.post("/issues/delete", async (req, res, next) => {
+  try{
   const issueId = parseInt(req.body.issueId);
 
-  db.collection('issues').deleteOne({id:issueId}).then((response) => {
+  const response = await Issue.deleteOne({id:issueId});
+
  if (response.deletedCount === 1) {
  res.redirect('/')
  } else {
- res.status(404).send('Error: No city found')
+ res.status(404).send('Error: No city found');
  }
-})
+  }
+  catch(err){
+    next(err);
+  }
 });
 
 
@@ -106,5 +147,3 @@ app.use((err,req,res,next)=> {
 })
 
 
-console.log(issues.dateCrea);
-console.log(issues.etat);
